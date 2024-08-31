06326fdfed9184ac5911de845c505d7ef5e50e8f
monalisadas2000 <143864885+monalisadas2000@users.noreply.github.com>
2024-08-31 11:58:46 +0530

    update

--- viewLeave.php
+++ viewLeave.php
@@ -70,7 +70,7 @@ session_start(); {
                                                 <th><?php echo $row["end_date"]; ?></th>
                                                 <th><?php echo $row["leave_duration"]; ?></th>
                                                 <th><?php echo $row["reason"]; ?></th>
-                                                <th><?php echo $row["leave_status"]; ?></th>
+                                                <!-- <th><?php echo $row["leave_status"]; ?></th> -->
                                                 <?php if ($em_role == '1') { ?>
                                                     <th>
                                                         <i class="fa-solid fa-pen-to-square me-2 ms-2 text-primary"></i>
