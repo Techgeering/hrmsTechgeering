a6979cf254bf7541f1939eaa1f9f50acde031f77
ASUTOSH DAS <143777655+Asutosh1963@users.noreply.github.com>
2024-09-09 15:20:48 +0530

    Update viewLeave.php

--- viewLeave.php
+++ viewLeave.php
@@ -2,7 +2,6 @@
 session_start(); {
     $em_role = $_SESSION["em_role"];
     $emp_id = $_SESSION["emp_id"] ;
-    echo "<script>alert( $emp_id)</script>"
 }
 ?>
 <!DOCTYPE html>
@@ -192,7 +191,7 @@ session_start(); {
                     <div class="modal-body">
                         <div class="form-group">
                             <label for="empId">Employee id</label>
-                            <input type="text" class="form-control" id="empId" name="empId" value = "<?php echo  $emp_id; ?>">
+                            <input type="text" class="form-control" id="empId" name="empId" value = "<?php echo  $emp_id; ?>" readonly>
                         </div>
                         <div class="form-group">
                             <label for="Leavetype">Leave type</label>
