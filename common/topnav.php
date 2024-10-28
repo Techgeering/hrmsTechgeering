5a9320520b5cad968162eaa57f87d7d6d6dce4c5
monalisadas2000 <143864885+monalisadas2000@users.noreply.github.com>
2024-10-28 19:17:30 +0530

    updated

--- common/topnav.php
+++ common/topnav.php
@@ -25,9 +25,15 @@ if ($userid === NULL) {
 
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-success">
     <!-- Navbar Brand-->
-    <a class="navbar-brand ps-3 text-center" href="index.php">
-        <img src="assets/img/logo.png" class="img-fluid">
-    </a>
+    <?php if ($em_role == '1') { ?>
+        <a class="navbar-brand ps-3 text-center" href="index.php">
+            <img src="assets/img/logo.png" class="img-fluid">
+        </a>
+    <?php } else { ?>
+        <a class="navbar-brand ps-3 text-center" href="javascript:void(0)">
+            <img src="assets/img/logo.png" class="img-fluid">
+        </a>
+    <?php } ?>
     <!-- Sidebar Toggle-->
     <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i
             class="fas fa-bars"></i></button>
